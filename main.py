1433fd952229268aba42be7598a2b4ee52689c0d
nariman079 <nariman079i@mail.ru>
2024-05-14 22:32:36 +0300

    fix 5 method

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ This is docstring
 from services.main_services import (install_video_form_file,
                                 check_channel_last_video,
                                 download_video_in_youtube,
-                                get_video_url_from_channel)
+                                get_video_url_from_channel,
+                                )
+from services.youtube.v2 import DownloadVideo
 
 while True:
     try:
@@ -13,7 +15,7 @@ while True:
                            '2. Upload using urls.txt\n' 
                            '3. Get urls\n'
                            '4. Download video from url\nEnder download method (1,2,3 or 4):'))
-        if method in [1,2,3,4]:
+        if method in [1,2,3,4, 5]:
             break
     except TypeError as errors:
         print("Enter correnct value")
@@ -21,18 +23,16 @@ while True:
 
 match method:
     case 1:
-        # ✅
         channel_url = input("Enter channel url:") or 'https://www.youtube.com/@eminem/videos'
         check_channel_last_video(channel_url=channel_url)
     case 2:
         txt_file = input("Enter txt filename:") or '@eminem.txt'
         install_video_form_file(txt_file)
     case 3:
-        # ✅
         channel_url = input('Enter channel url:') or 'https://www.youtube.com/@eminem/videos'
         try:
             while True:
-                install_immediately = int(input("Install immediately? (1-True or 0-False): "))
+                install_immediately = int(input("Install immediately? (1. Yes or 0. No)\nEnter variant: "))
 
                 if install_immediately in [1,0]:
                     get_video_url_from_channel(
@@ -50,4 +50,4 @@ match method:
         video_url = input("Enter video url:") or 'https://www.youtube.com/watch?v=rR3PcMB1bNw'
         download_video_in_youtube(video_url=video_url, id_=1, all_video_count=1)
     case _:
-        pass
+        DownloadVideo().execute()
--- services/youtube/v2.py
+++ services/youtube/v2.py
@@ -3,8 +3,10 @@
 """
 from pathlib import Path
 import json
+import os
 
 from base_datas import base_json_file
+from utils.pytube.__main__ import YouTube
 
 def is_exists_file(file_name: str, path: Path) -> bool:
     """
@@ -43,3 +45,93 @@ def is_new_video(channel_name: str, video_id: str) -> bool:
         )
 
     return is_new
+
+class DownloadVideo:
+    """
+    Download video
+    """
+    def __init__(self) -> None:
+        self.video = YouTube(
+            input('Ender video url: ') or
+            'https://www.youtube.com/watch?v=N3onZdIIPh0'
+            )
+        self.path = Path('.', 'downloads')
+
+    def _check_is_exists_file(self) -> None:
+        """
+        Checking is exists file in path
+        """
+        if not os.path.isdir(self.path):
+            os.makedirs(self.path, exist_ok=True)
+
+        self.is_exists = is_exists_file(
+            self.video.title,
+            self.path
+        )
+
+    def _setting_download_video(self) -> None:
+        """
+        Settigns download video format
+        """
+        while True:
+            try:
+                self.type = int(input("Type:\n1. Video\n2. Audio\nEnter variant (1 or 2) [default: 1]: ") or  1)
+                if self.type in [1, 2]:
+                    break
+                else:
+                    raise TypeError()
+            except TypeError:
+                print("Enter correct value!\n")
+            except ValueError:
+                print("Enter correct value!\n")
+
+    def _check_video_type(self) -> None:
+        """
+        Check video type
+        """
+        while True:
+            try:
+                if self.type == 1:
+                    self.video_quality = int(input('Quality:\n1. 1080p\n2. 720p\nEnter variant (1 or 2) [default: 2]: ') or 2)
+                if self.type in [1, 2]:
+                    break
+                else:
+                    raise TypeError()
+            except TypeError:
+                print("Enter correct value!\n")
+            except ValueError:
+                print("Enter correct value!\n")
+        
+    def _generate_filters(self)-> None:
+        """
+        Generate filters for search in Youtube
+        """
+        self.filter_kwargs = dict()
+        if self.type == 2:
+            self.filter_kwargs['only_audio'] = True
+            self.path = self.path / Path('audios')
+        elif self.type == 1:
+            self.filter_kwargs['resolution'] = '1080p' if self.video_quality == 1 else '720p'
+            self.path = self.path / Path('videos')
+        print(self.filter_kwargs)
+        if not os.path.isdir(self.path):
+                os.makedirs(self.path, exist_ok=True)
+
+    def _download_file(self) -> None:
+        """
+        download file
+        """
+        print("Instalation file")
+        if video_for_download := self.video.streams.filter(**self.filter_kwargs).first():
+            video_for_download.download(output_path=self.path)
+
+    def execute(self) -> None:
+        """
+        Run command
+        """
+        self._check_is_exists_file()
+        if not self.is_exists:
+            self._setting_download_video()
+            self._check_video_type()
+            self._generate_filters()
+            self._download_file()
